9b57c02a7b480b9975a281b54e81dedb5dff6d55
8MyrCSUMB <gpillegand@csumb.edu>
2026-04-14 19:35:22 -0700

    start final project

---
+++ index.mjs
@@ -0,0 +1,33 @@
+import express from 'express';
+import mysql from 'mysql2/promise';
+import 'dotenv/config';
+const app = express();
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+//for Express to get values using the POST method
+app.use(express.urlencoded({ extended: true }));
+//setting up database connection pool, replace values in red
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWD,
+    database: process.env.DB_DATABASE,
+    connectionLimit: 10,
+    waitForConnections: true
+});
+//routes
+app.get('/', (req, res) => {
+    res.send('Hello Express app!')
+});
+app.get("/dbTest", async (req, res) => {
+    try {
+        const [rows] = await pool.query("SELECT CURDATE()");
+        res.send(rows);
+    } catch (err) {
+        console.error("Database error:", err);
+        res.status(500).send("Database error!");
+    }
+});//dbTest
+app.listen(3000, () => {
+    console.log("Express server running")
+})
